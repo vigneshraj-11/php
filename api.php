fb8676473901376b1adf5154df9dab0b08ebec8a
Vignesh <vigneshraj.jul11@gmail.com>
2025-03-02 09:59:37 +0530

    Chnages

--- api.php
+++ api.php
@@ -1,10 +1,3 @@
 <?php
-header("Content-Type: application/json");
-header("Access-Control-Allow-Origin: *");
-
-if ($_SERVER["REQUEST_METHOD"] === "GET") {
-    echo json_encode(["message" => "Hi Code invoking Successful"]);
-} else {
-    echo json_encode(["message" => "Invalid Request"]);
-}
+echo "Hello, PHP is working!";
 ?>
